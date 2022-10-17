533864060cb3f33817b7bfd126e955094bc05013
성승연 <alwayssingyo@gmail.com>
2022-10-17 17:20:18 +0900

    test : 커밋 테스트

--- src/components/DetailContainer/index.js
+++ src/components/DetailContainer/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const DetailInner = styled.div `
 	margin-top: 64px;
   @media screen and (max-width: 768px){
-		margin-top: 56px;
+		margin-top: 56px; 
 	}
 `;
 
